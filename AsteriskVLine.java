4a24e91e6a62c747c8b65086bc97c8f044c6b777
Chankaya <ankshu2015@gmail.com>
2017-02-13 16:09:51 +0000

    Create AsteriskVLine.java

---
+++ AsteriskVLine.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class AsteriskVLine
+{
+    public static void main(String args[])throws IOException
+    {
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     System.out.println("Enter the no. of asterisk to be displayed in a line:");
+     int n=Integer.parseInt(br.readLine());
+     for (int i=n;i>0;i--)
+     System.out.println("*");
+    }
+}
